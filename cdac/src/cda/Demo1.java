3e759fb721a1a62e7b95417a70e73c3511446e61
Sanket Pawar <pawarsanket838@gmail.com>
2023-06-26 17:28:57 +0530

    msg

---
+++ cdac/src/cda/Demo1.java
@@ -0,0 +1,11 @@
+package cda;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is the main method...");
+	}
+
+}
